d5aae5e5d322c7e71c1f02023a0601994daa81b7
Bill Fenner <fenner@gmail.com>
2022-11-25 08:41:46 -0800

    apps: snmpset: allow SET with NULL varbind for testing

--- apps/snmpset.c
+++ apps/snmpset.c
@@ -179,6 +179,7 @@ main(int argc, char *argv[])
             case 'x':
             case 'd':
             case 'b':
+            case 'n': /* undocumented */
 #ifdef NETSNMP_WITH_OPAQUE_SPECIAL_TYPES
             case 'I':
             case 'U':
